a154ec25c9c257880afa7622fe3e4abc49fe4f0a
Pooja Maheshwari <pooja.maheshwari247@gmail.com>
2024-12-01 12:16:37 +0200

    build and add out dir

---
+++ out/_next/static/wwLmNNPGXacxcX0MVtkgO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-acb30c580916b7c2.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
