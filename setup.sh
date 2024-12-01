1927e2846d4542914d4d977b0efd4bddcdadf72e
Pooja Maheshwari <pooja.maheshwari247@gmail.com>
2024-12-01 11:46:28 +0200

    Fix build files

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 # Install Node.js
 curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
 apt-get install -y nodejs
 
-# Install dependencies and build the app
+# Install dependencies and build the Next.js app
 npm install
 npm run build
